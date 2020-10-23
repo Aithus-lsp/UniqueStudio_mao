bceef8f681a358142b9247a11c7814f9339769fc
Aithus-lsp <aithus@163.com>
2020-10-23 17:57:01 +0800

    写出损失函数大概

--- 决策树/决策树_ID3.py
+++ 决策树/决策树_ID3.py
@@ -3,6 +3,7 @@
 
 这是关于决策树尝试实验的文档，打算先写ID3，写其它树时会从这里import,但是这里的注释不会使用模块的标准注释。
 吐槽一下，我在好久之前就在图书馆尝试构造，纯粹瞎摸，最后无功而返。
+这次在定义类的时候直接将数据使用。
 注释待续
 """
 
@@ -16,30 +17,50 @@ import pandas as pd
 data = pd.read_csv("./diabetes.csv")
 
 
+#处理数据缺失值，但好像没有啊。
+data.dropna(inplace=True)
+
+
+#害怕出错，定义一些通用函数
+#计算熵,以e为底,使用极大似然估计,输入为一个series
+    def cal_entropy(series):
+        val_count = series.value_counts()
+        entropy = np.sum(np.log(val_count) * val_count) / np.sum(val_count)
+        return entropy
+
+
 class DecisionTree(object):
 """一颗通用决策树。
 不对特征进行选择，丢弃。
 它将保存一些基础属性，函数，有：
 """
 
+    
     def __init__(self, algor=None, Thresh=(0.01,10), ThreshVal=0.5):
         """一棵树的基本要素，分别为阀值,"""
         self.__algor = 
         self.__Thresh =
         self.__ThreshVal =
 
-    #寻找分界点
-    #计算熵,以e为底,使用极大似然估计,输入为一个series
-    def cal_entropy(self, series):
-        entropy = sum([])
-        return entropy
-    def cal_bene(self):
-    """输入数据计算信息增益"""
-        bene_dict={}
-        out_bene = cal_entropy(data.Outcome)
-        for x in data.index:
-            bene_dict[x] = cal_entropy(series)
-        return bene_dict
+    
+    #寻找分界点，使用信息增益比
+    
+    #输入数据计算信息增益
+    bene_dict={}
+    #计算信息熵
+    out_bene = cal_entropy(data.Outcome)
+    #生成信息增益字典
+    for x in data: 
+        new_series = pd.Series(data["Outcome"].values, data[x].values)
+        new_group = new_series.groupby(x)
+        group_entr = new_group.agg(cal_entropy)
+        group_count = new_group.count()
+        entropy = np.sum(group_entr.values / group_count.values)
+        bene_dict[x] = entropy
+    
+    #生成
+
+    
         
         
     def ChooseBest(self, ):
